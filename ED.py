278e9f1a710138dccdfa883b5cce495d91a6eac2
Max(Shangru) Li <max.shangru.li@gmail.com>
2020-01-05 16:56:21 -0500

    Updated to version 2.0
    Re-structured the algorithm, fixed long line bug, encrypted cipher now contains both upper and lower case letter.

--- ED.py
+++ ED.py
@@ -1,135 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A simple script to encrypt and decrypt strings.
+"""
+
 import random
 
-seed_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z']
-seed_number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-seed_symbol = ['`', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', '{', ']', '}', '|', ';',
-               ':', '>', ',', '<', '.', '/', '?']
+__author__ = "Shangru Li"
+__copyright__ = "Copyright 2020, Shangru Li"
+__credits__ = "Shangru Li"
+__license__ = "MIT"
+__version__ = "2.0"
+__maintainer__ = "Shangru Li"
+__email__ = "max.shangru.li@gmail.com"
+__status__ = "Stable"
 
-def encrypt(arg):
-	# assign input string to `text` 
-    text = arg
-    # `check' is for remembering if the input string is code or seed
-    check = 0
-    # if the first character of the input is '!' then it's the seed
-    if arg[0] == '!':
-        check = 1
-        text = arg.replace('!', '')
-    # initialize code
-    code = ""
-    # initialize a random offset of range (10,99)
-    # offset is added to the character's
-    offset = random.randint(10, 99)
-    # set length of the input string
-    length = len(text)
+###############################___Seeds___######################################
+seedLetter =[
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+	'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'
+	]
+seedNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+seedSymbol = [
+	'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+',
+	'=', '[', '{', ']', '}', '|', ';', ':', '>', ',', '<', '.', '/', '?'
+	]
+################################################################################
 
-	# check for input's length, if it's greater than 9,
-	# i.e., it's double digit, then we want to set a indicator in
-	# front of the seed, indicator is randomly selected from 'seed_letter'
-    if length > 9:
-        indicator = seed_letter[random.randint(0, len(seed_letter) - 1)]
-        seed = indicator + str(length) + str(offset)
-    else:
-        seed = str(length) + str(offset)
-    # looping over every character in the input string
-    for n in text:
-    	# first randomly pick a letter from 'seed_letter'
-        seed1 = seed_letter[random.randint(0, len(seed_letter) - 1)]
-        # then randomly pick a number from 'seed_number'
-        seed2 = seed_number[random.randint(0, len(seed_number) - 1)]
-        # finally randomly pick a symbol from 'seed_symbol'
-        seed3 = seed_symbol[random.randint(0, len(seed_symbol) - 1)]
-        # combine the three random seeds to one
-        seed_combine = seed1 + seed2 + seed3
-        # the initial code consists of:
-        # "" + of character plus 'offset' + combination of three seeds
-        code = code + str(ord(n) + offset) + seed_combine
-        # remember the random seeds
-        seed = seed + seed_combine
-    # check = 1 means we have encoded the seed
-    if check == 1:
-    	# then we return the full code
-        return seed + code + '!'
-    # we encode the seed and put an indicator '!' in front of it
-    return encrypt('!' + seed) + code
+def main():
+	seedIndicator = generateSeedIndicator()
+	textToEncrypt = input("Enter: ")
+	encryptedText = encrypt(textToEncrypt, seedIndicator)
+	print("Encrypt: " + encryptedText)
+	print("Decrypt: " + decrypt(encryptedText, seedIndicator) + '\n')
 
+def generateSeedIndicator():
+	"""
+	Generate a special character to separate the seed and code from the `seed_symbol`
+	"""
+	seedIndicator = seedSymbol[random.randint(0, len(seedSymbol) - 1)]
+	seedSymbol.remove(seedIndicator)
+	return seedIndicator
 
-def decrypt(arg):
-	# assign input string to 'cypher'
-    cypher = arg
-    # try to locate the seed/code separation indicator
-    if arg.find('!') != -1:
-    	# split seed and code by separator '!'
-        seed_code, code_code = arg.split('!')
-        # decrypt the seed first
-        seed_code = decrypt(seed_code)
-        # combine decrypt seed and code
-        # at this point cyhper should not contain '!'
-        # hence, cypher.has('!') ? seed not decoded
-        cypher = seed_code + code_code
-    # `char` is the result string
-    chars = []
-    # `trans` is used to store the character to decode
-    trans = []
-    check = 0
-    header = 0
-    seed_index = 0
-    seed_num = 3
-    # check the first character of the cypher
-    # if it's a digit, then the actual result string is single digit length
-    if cypher[0].isdigit():
-    	# `header` is the length of `length` and `offset`
-        header = 3
-        # `length` is the first index
-        length = int(cypher[0])
-        # `offset` is the sum of the first and second index after `length`
-        offset = int(cypher[1] + cypher[2])
-        # `seed` is the 'length' * 'seed_num' without the "header"-`length` and `offset`
-        seed = cypher[3:length * seed_num + header]
-        # `code` is what's left after `seed`
-        code = cypher[length * seed_num + header:]
-    else:
-    	# case where the result string's length is double digit
-        # the first index is a indicator, hence real cypher starts index 5
-        header = 5
-        # first index is indicator, length is double digit
-        length = int(cypher[1] + cypher[2])
-        # rest is the same
-        offset = int(cypher[3] + cypher[4])
-        seed = cypher[5:length * seed_num + header]
-        code = cypher[length * seed_num + header:]
-    # go through each character in `code`
-    for line in code:
-        for c in line:
-        	# separate the three seeds
-            seed1 = seed[seed_index]
-            seed2 = seed[seed_index + 1]
-            seed3 = seed[seed_index + 2]
-            # current index points to `seed1`, increment `check` go to next character
-            if c == seed1 and check == 0:
-                check = 1
-            # current index points to `seed2`, do the same
-            elif c == seed2 and check == 1:
-                check = 2
-            # current index points to `seed3`, we can start decrypting
-            elif c == seed3 and check == 2:
-            	# each original character is the ASCII code - offset
-                chars.append((chr(int(''.join(trans)) - offset)))
-                # clear the array after the have decrypted
-                trans.clear()
-                # proceed to next character
-                check = 0
-                seed_index = seed_index + seed_num
-            else:
-            	# index points to the encrypted char, store it for decrypt
-                trans.append(c)
-    # return the result output
-    return ''.join(chars)
-    
-##############---Message_Display_Input_Handel---#####################
+def getRandomCaseSeedLetter():
+	"""
+	Randomly pick a either uppercase or lowercase letter from `seedLetter`
+	"""
+	seed = random.randint(0, 1)
+	if seed == 0:
+		return seedLetter[random.randint(0, len(seedLetter) - 1)]
+	else:
+		return str.capitalize(seedLetter[random.randint(0, len(seedLetter) - 1)])
 
-input_str = input("Enter: ")
-result = encrypt(input_str)
-print("Encrypt: " + result)
-print("Decrypt: " + decrypt(result) + '\n')
-#print("Encrypt_input: " + result[len(input_str * 3) + 3:] + '\n' + "Encrypt_seed: " + result[:len(input_str) * 3 + 3])
+def countNumOfOccurrences(list, x):
+	"""
+	Count the number of occurrences of `x` in `list`
+	"""
+	return list.count(x)
+
+def encrypt(textToEncrypt, seedIndicator):
+	# If the first character of the input is `seedIndicator` then it's the seed
+	if textToEncrypt[0] == seedIndicator:
+		# Remove the `seedIndicator`
+		inputText = textToEncrypt.replace(seedIndicator, '')
+	else:
+		inputText = textToEncrypt
+	code = ""
+	# Initialize a random offset to be added to the character's ASCII code
+	offset = random.randint(10, 99)
+	# The first two characters of the `seed` is the `offset`
+	seed = str(offset)
+	for n in inputText:
+		# Randomly pick a letter from 'seed_letter', upper or lower case
+		randomLetter = getRandomCaseSeedLetter()
+		# Randomly pick a number from 'seed_number'
+		randomNumber = seedNumber[random.randint(0, len(seedNumber) - 1)]
+		# Randomly pick a symbol from 'seed_symbol'
+		randomSymbol = seedSymbol[random.randint(0, len(seedSymbol) - 1)]
+		seed_combine = randomLetter + randomNumber + randomSymbol
+		# Appending to the seed
+		seed = seed + seed_combine
+		# The result is:
+		# ASCII code of character plus 'offset' + combination of three seeds
+		code = code + str(ord(n) + offset) + seed_combine
+	if textToEncrypt[0] == seedIndicator:
+		# If the first character is the `seedIndicator`, then we just
+		# encrypted the seed. Hence we return the full encrypted input.
+		return seed + seedIndicator + code + seedIndicator
+	else:
+		# Else we encode the seed and put an indicator seedIndicator in front of it
+		return encrypt(seedIndicator + seed, seedIndicator) + code
+
+def decrypt(textToDecrypt, seedIndicator):
+	# There should be 2 occurrences of `seedIndicator` in the `textToDecrypt`
+	# The `textToDecrpt` should looks like this:
+	# {seed + seedIndicator + encryptedSeed} + {seedIndicator + encryptedInput}
+	if countNumOfOccurrences(textToDecrypt, seedIndicator) == 2:
+		# Split the input by `seedIndicator` to three parts
+		seedForEncryptedSeed, encryptedSeed, encryptedInput = textToDecrypt.split(seedIndicator)
+		# Decrpt the seed for user input
+		decryptedSeed = decrypt(seedForEncryptedSeed + seedIndicator + encryptedSeed, seedIndicator)
+		# Combine the decrypted seed with encrypted input for further encrypt
+		textToDecrypt = decryptedSeed + seedIndicator + encryptedInput
+	# The first two characters of seed is the `offset`
+	offset = int(textToDecrypt[0] + textToDecrypt[1])
+	# Remove the `offset` from `textToDecrpt`
+	textToDecrypt = textToDecrypt[2:len(textToDecrypt)]
+	# Split the textToDecrypt by seedIndicator to get the seed and code
+	seed, code = textToDecrypt.split(seedIndicator)
+	result = []
+	# This is to remember the ASCII code for a paticular character
+	# For example, ASCII code 102, in this list: ['1', '0', '2']
+	asciiList = []
+	# Check which seed the index is pointing at
+	check = 0
+	# The index for seed list
+	seedIndex = 0
+	for i in range(len(code)):
+		# separate the three seeds
+		seed1 = seed[seedIndex]
+		seed2 = seed[seedIndex + 1]
+		seed3 = seed[seedIndex + 2]
+		# current index points to `seed1`, increment `check` go to next character
+		if code[i] == seed1 and check == 0:
+			check = 1
+		# current index points to `seed2`, do the same
+		elif code[i] == seed2 and check == 1:
+			check = 2
+		# current index points to `seed3`, we can start decrypting
+		elif code[i] == seed3 and check == 2:
+			# each original character is the ASCII code - offset
+			result.append((chr(int(''.join(asciiList)) - offset)))
+			# clear the array after the have decrypted
+			asciiList.clear()
+			# Go to the next set of seed
+			seedIndex += 3
+			check = 0
+		else:
+			# index points to the encrypted char, store it for decrypt
+			asciiList.append(code[i])
+	return ''.join(result)
+
+if __name__ == "__main__":
+	main()
