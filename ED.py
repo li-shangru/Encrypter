fe9fbd04264c7be9683ece55bb52bc70cbb8e1d0
Shangru Li <maxsli@protonmail.com>
2020-11-29 17:04:32 -0500

    Updated to version 3.2
    Added typing, minor improvements

--- ED.py
+++ ED.py
@@ -12,17 +12,17 @@ __author__ = "Shangru Li"
 __copyright__ = "Copyright 2020, Shangru Li"
 __credits__ = "Shangru Li"
 __license__ = "MIT"
-__version__ = "3.1"
+__version__ = "3.2"
 __maintainer__ = "Shangru Li"
 __email__ = "maxsli@protonmail.com"
 __status__ = "Stable"
 
 ###############################___Seeds___######################################
-seed_letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
-            "q","r","s","t","u","v","w","x","y","z",]
-seed_number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-seed_symbol = ["`", "~","!","@","#","$","%","^","&","*","(",")","-","_","+","=",
-            "[","{","]", "}","|",";",":",">",",","<",".","/","?",]
+seed_letter: list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+                    "q","r","s","t","u","v","w","x","y","z",]
+seed_number: list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+seed_symbol: list = ["`", "~","!","@","#","$","%","^","&","*","(",")","-","_","+","=",
+                    "[","{","]", "}","|",";",":",">",",","<",".","/","?",]
 ###############################___Args___#######################################
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--decrypt", help="Decrypt an encrypted string", type=str)
@@ -37,7 +37,7 @@ def main():
     if args.decrypt is not None:
         try:
             print("Decrypting: " + args.decrypt)
-            decrypted_input = decrypt(args.decrypt, False)
+            decrypted_input: str = decrypt(args.decrypt)
             print("Decrypted: " + decrypted_input + "\n")
         except SyntaxError:
             print(
@@ -46,22 +46,22 @@ def main():
             )
     elif args.encrypt is not None:
         print("Encrypting: " + args.encrypt)
-        encrypted_input = encrypt(args.encrypt, False)
+        encrypted_input: str = encrypt(args.encrypt)
         print("Encrypted: " + encrypted_input + "\n")
     else:
         # Encrypt
-        text_to_encrypt = input("Enter text to encrypt: ")
+        text_to_encrypt: str = input("Enter text to encrypt: ")
         print("Encrypting: " + text_to_encrypt)
-        encrypted_text = encrypt(text_to_encrypt, False)
+        encrypted_text: str = encrypt(text_to_encrypt)
         print("Encrypted: " + encrypted_text + "\n")
         # Decrypt
-        text_to_decrypt = input(
+        text_to_decrypt: str = input(
             "Enter text to decrypt, or press enter to decrpt the above text: "
         )
         try:
             if not text_to_decrypt:
                 text_to_decrypt = encrypted_text
-            decrypted_text = decrypt(text_to_decrypt, False)
+            decrypted_text: str = decrypt(text_to_decrypt)
             print("Decrypting: " + text_to_decrypt)
             print("Decrypted: " + decrypted_text + "\n")
         except SyntaxError:
@@ -69,54 +69,56 @@ def main():
                 "Decryption failed, please make sure the encrypted text is correct."
                 + "\n"
             )
-    user_command = input("Enter R to run the program again, or anything else to exit: ")
+    user_command: str = input("Enter R to run the program again, or anything else to exit: ")
     if user_command == "R" or user_command == "r":
         print("\n")
         main()
 
 
-def generate_seed_indicator():
+def generate_seed_indicator() -> str:
     """
     Generate a special character to separate the seed and code from the `seed_symbol`.
     """
-    seed_indicator = seed_symbol[random.randint(0, len(seed_symbol) - 1)]
+    seed_indicator: str = seed_symbol[random.randint(0, len(seed_symbol) - 1)]
     seed_symbol.remove(seed_indicator)
     return seed_indicator
 
 
-def get_random_case_seedletter():
+def get_random_case_seedletter() -> str:
     """
     Randomly pick a either uppercase or lowercase letter from `seedLetter`.
     """
-    seed = random.randint(0, 1)
-    if seed == 0:
+    if random.randint(0, 1) == 0:
         return seed_letter[random.randint(0, len(seed_letter) - 1)]
     else:
         return str.capitalize(seed_letter[random.randint(0, len(seed_letter) - 1)])
 
 
-def encrypt(text_to_encrypt, is_seed):
+def encrypt(text_to_encrypt: str, is_seed: bool = False) -> str:
+    """
+    Encrypt the given string `text_to_encrypt` and return the result.
+    """
     if is_seed == False:
         # Pick a random seedIndicator
-        seed_indicator = generate_seed_indicator()
+        seed_indicator: str = generate_seed_indicator()
     else:
         seed_indicator = text_to_encrypt[0]
         # Remove the `seedIndicator`
         text_to_encrypt = text_to_encrypt.replace(seed_indicator, "")
-    code = ""
+    code: str = ""
     # Initialize a random offset to be added to the character's ASCII code
-    offset = random.randint(10, 99)
+    offset: int = random.randint(10, 99)
     # The first two characters of the `seed` is the `offset`
-    seed = str(offset)
+    seed: str = str(offset)
     for n in text_to_encrypt:
         # Randomly pick a letter from 'seed_letter', upper or lower case
-        random_letter = get_random_case_seedletter()
+        random_letter: str = get_random_case_seedletter()
         # Randomly pick a number from 'seed_number'
-        random_number = seed_number[random.randint(0, len(seed_number) - 1)]
+        random_number: str = seed_number[random.randint(0, len(seed_number) - 1)]
         # Randomly pick a symbol from 'seed_symbol'
-        random_symbol = seed_symbol[random.randint(0, len(seed_symbol) - 1)]
+        random_symbol: str = seed_symbol[random.randint(0, len(seed_symbol) - 1)]
         # Appending to the seed
-        seed_combine = random_letter + random_number + random_symbol
+        seed_combine: str = random_letter + random_number + random_symbol
         # Appending to the seed
         seed = seed + seed_combine
         # The code is:
@@ -135,10 +137,13 @@ def encrypt(text_to_encrypt, is_seed):
         )
 
 
-def decrypt(text_to_decrypt, is_seed):
+def decrypt(text_to_decrypt: str, is_seed: bool = False) -> str:
+    """
+    Decrypt the given cypher `text_to_decrypt` and return the result.
+    """
     try:
         # The last character is the `seedIndicator`
-        seed_indicator = text_to_decrypt[len(text_to_decrypt) - 1]
+        seed_indicator: str = text_to_decrypt[len(text_to_decrypt) - 1]
         # Removing the `seedIndicator` from input
         text_to_decrypt = text_to_decrypt[:-1]
         # The `textToDecrpt` should looks like this:
@@ -152,7 +157,7 @@ def decrypt(text_to_decrypt, is_seed):
                 encrypted_input,
             ) = text_to_decrypt.split(seed_indicator)
             # Decrpt the seed for user input
-            decrypted_seed = decrypt(
+            decrypted_seed: str = decrypt(
                 seed_for_encrypted_seed
                 + seed_indicator
                 + encrypted_seed
@@ -162,24 +167,24 @@ def decrypt(text_to_decrypt, is_seed):
             # Combine the decrypted seed with encrypted input for further encrypt
             text_to_decrypt = decrypted_seed + seed_indicator + encrypted_input
         # The first three characters of seed is the `offset`
-        offset = int(text_to_decrypt[0] + text_to_decrypt[1])
+        offset: int = int(text_to_decrypt[0] + text_to_decrypt[1])
         # Remove the `offset` from `textToDecrpt`
         text_to_decrypt = text_to_decrypt[2 : len(text_to_decrypt)]
         # Split the textToDecrypt by seedIndicator to get the seed and code
         seed, code = text_to_decrypt.split(seed_indicator)
-        result = []
+        result: list = []
         # This is to remember the ASCII code for a paticular character
         # For example, ASCII code 102, in this list: ['1', '0', '2']
-        ascii_list = []
+        ascii_list: list = []
         # Check which seed the index is pointing at
-        check = 0
+        check: int = 0
         # The index for seed list
-        seed_index = 0
+        seed_index: int = 0
         for i in range(len(code)):
             # separate the three seeds
-            seed1 = seed[seed_index]
-            seed2 = seed[seed_index + 1]
-            seed3 = seed[seed_index + 2]
+            seed1: str = seed[seed_index]
+            seed2: str = seed[seed_index + 1]
+            seed3: str = seed[seed_index + 2]
             # current index points to `seed1`, increment `check` go to next character
             if code[i] == seed1 and check == 0:
                 check = 1
--- ED_test.py
+++ ED_test.py
@@ -5,7 +5,7 @@
 Test file for ED.py
 """
 
-import pytest
+import pytest # type: ignore
 import random
 import string
 import importlib
@@ -14,59 +14,59 @@ import ED
 
 def test_number():
     """
-    Test 16 random generated numbers, each number in range (-x^128, x^128)
+    Test 16 random generated numbers, each number in range (-x^128, x^128).
     """
     for x in range(16):
         importlib.reload(ED)
-        random_number = str(random.randint(-(x ** 128), x ** 128))
-        encrypted_result = ED.encrypt(random_number, False)
-        decrypted_result = ED.decrypt(encrypted_result, False)
+        random_number: str = str(random.randint(-(x ** 128), x ** 128))
+        encrypted_result: str = ED.encrypt(random_number)
+        decrypted_result: str = ED.decrypt(encrypted_result)
         assert random_number == decrypted_result
 
 
 def test_string():
     """
-    Test 16 random generated letters, length of each letter is in range (0, x^4)
+    Test 16 random generated letters, length of each letter is in range (0, x^4).
     """
     for x in range(16):
         importlib.reload(ED)
-        letters = string.ascii_letters
-        random_letters = "".join(
+        letters: str = string.ascii_letters
+        random_letters: str = "".join(
             random.choice(letters) for i in range(random.randint(0, x ** 4))
         )
-        encrypted_result = ED.encrypt(random_letters, False)
-        decrypted_result = ED.decrypt(encrypted_result, False)
+        encrypted_result: str = ED.encrypt(random_letters)
+        decrypted_result: str = ED.decrypt(encrypted_result)
         assert random_letters == decrypted_result
 
 
 def test_character():
     """
-    Test 16 random generated special characters, length of each character is in range (0, x^4)
+    Test 16 random generated special characters, length of each character is in range (0, x^4).
     """
     for x in range(16):
         importlib.reload(ED)
-        characters = string.punctuation
-        random_character = "".join(
+        characters: str = string.punctuation
+        random_character: str = "".join(
             random.choice(characters) for i in range(random.randint(0, x ** 4))
         )
-        encrypted_result = ED.encrypt(random_character, False)
-        decrypted_result = ED.decrypt(encrypted_result, False)
+        encrypted_result: str = ED.encrypt(random_character)
+        decrypted_result: str = ED.decrypt(encrypted_result)
         assert random_character == decrypted_result
 
 
 def test_characters_letters_numbers():
     """
     Test 16 random generated string consist of special characters, letters and digits,
-    Of length in range 0 to x^4
+    Of length in range 0 to x^4.
     """
     for x in range(16):
         importlib.reload(ED)
-        mixed_input = string.ascii_letters + string.digits + string.punctuation
-        random_input = "".join(
+        mixed_input: str = string.ascii_letters + string.digits + string.punctuation
+        random_input: str = "".join(
             random.choice(mixed_input) for i in range(random.randint(0, x ** 4))
         )
-        encrypted_result = ED.encrypt(random_input, False)
-        decrypted_result = ED.decrypt(encrypted_result, False)
+        encrypted_result: str = ED.encrypt(random_input)
+        decrypted_result: str = ED.decrypt(encrypted_result)
         assert random_input == decrypted_result
 
 
@@ -77,9 +77,9 @@ def test_error_input():
     """
     for x in range(100):
         importlib.reload(ED)
-        mixed_input = string.ascii_letters + string.digits + string.punctuation
-        random_input = "".join(
+        mixed_input: str = string.ascii_letters + string.digits + string.punctuation
+        random_input: str = "".join(
             random.choice(mixed_input) for i in range(random.randint(0, x))
         )
         with pytest.raises(SyntaxError):
-            ED.decrypt(random_input, False)
+            ED.decrypt(random_input)
